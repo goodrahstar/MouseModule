5a4250130e142adfd4935c41f50f5f61d86403d7
Rahul Kumar <goodrahstar@gmail.com>
2015-10-18 16:32:43 +0530

    write in file

--- mouse.js
+++ mouse.js
@@ -1,9 +1,17 @@
 var mouse = require('osx-mouse')();
-
+var infile = require('fs');
 mouse.on('move', fuction(x, y) {
     //console.log(x, y);
     if (x != null) {
-      console.log('1');
+
+      infile.writeFile("/tmp/", "1", function(err) {
+          if(err) {
+              return console.log(err);
+          }
+
+//          console.log("The file was saved!");
+      });
+
     }
 
 
