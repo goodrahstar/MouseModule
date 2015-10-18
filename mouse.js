e0043879d0f1f126630d4cede34863c61aca0a73
Rahul Kumar <goodrahstar@gmail.com>
2015-10-19 05:02:56 +0530

    added file storing

--- mouse.js
+++ mouse.js
@@ -1,17 +1,18 @@
 var mouse = require('osx-mouse')();
 var infile = require('fs');
+var wstream =  infile.createWriteStream('output.txt');
+
 mouse.on('move', fuction(x, y) {
     //console.log(x, y);
     if (x != null) {
-
-      infile.writeFile("/tmp/", "1", function(err) {
-          if(err) {
-              return console.log(err);
-          }
-
+          wstream.write('1\n');
+//      infile.writeFile("/tmp/test.txt", "1", function(err) {
+  //        if(err) {
+    //                return console.log(err);
+      //            }
 //          console.log("The file was saved!");
-      });
-
+      //});
+wstream.end();
     }
 
 
